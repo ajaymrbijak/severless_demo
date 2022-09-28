0eb82e62a7bb1f9e362edcb7866f19ca1b12b7f7
ajaymrbijak <ajay.madireddy@bijak.in>
2022-09-28 23:17:48 +0530

    Ne Rest Api's

--- .build/src/handler.js
+++ .build/src/handler.js
@@ -9,8 +9,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUserViaId = exports.updateUser = exports.deleteUserResp = exports.postUserResp = exports.getUsers = void 0;
+exports.usersList = exports.currentTime = exports.getUserViaId = exports.updateUser = exports.deleteUserResp = exports.postUserResp = exports.getUsers = void 0;
 const db_manager_1 = require("./db_manager/db_manager");
+const axios_1 = require("axios");
 const app_1 = require("./model/app");
 // import { APIGatewayProxyEvent } from '/Users/bindureddykr/Desktop/Aws_Serverless_Demo_Code/nodejs/node_modules/@types/aws-lambda/index';
 function getUsers(event) {
@@ -277,4 +278,167 @@ function getUserViaId(event) {
     });
 }
 exports.getUserViaId = getUserViaId;
+// export async function restApi(event:APIGatewayProxyEvent){
+//   try{
+//     // let db= new DBManager();
+//     // db.createDBConnection([User])
+//     let url ='https://jsonplaceholder.typicode.com/todos'
+//     let data= new Promise((resolve, reject)=>{
+//       let request=https.get(url,response=>{
+//         let rawData='';
+//         response.on('data',reqData=>{
+//           rawData=rawData+reqData
+//         });
+//         response.on('end',()=>{
+//           try{
+//             resolve(JSON.stringify(rawData))
+//         }
+//         catch(e){
+//        reject(new Error(e))
+//         }
+//         });
+//       })
+//       request.on('Error',(err:string|undefined)=>{
+//         reject(new Error(err))
+//       });
+//     })
+//     return {
+//       statusCode: 200,
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Credentials": true,
+//       },
+//       body: JSON.stringify({
+//         msg: "Fetched data successfully..",
+//         data: data,
+//       }),
+//     };
+//   }
+//   catch(e){
+//     console.log("Exception due to :",e)
+//     const response = {
+//       statusCode: 500,
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Credentials": true,
+//       },
+//       body: JSON.stringify({ msg: "Internal Server Error", data: e.message }),
+//     };
+//     return response;
+//   }
+// }
+function currentTime(event) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let response = {
+            statusCode: 200,
+            hearder: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true,
+            },
+            body: JSON.stringify({
+                message: `Hello, the current time is ${new Date().toTimeString()}.`,
+            })
+        };
+        return response;
+    });
+}
+exports.currentTime = currentTime;
+// export async function httpApi(event:any)  {
+//   try {
+//     const result = await getRequest();
+//     console.log('result is: ', result);
+//     return {
+//       statusCode: 200,
+//       headers: {
+//         'Content-Type': 'application/json',
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Credentials": true,
+//     },
+//       body: JSON.stringify(result),
+//     };
+//   } catch (error) {
+//     console.log('Error is: ', error);
+//     return {
+//       statusCode: 400,
+//       body: error.message,
+//     };
+//   }
+// };
+// function getRequest() {
+//   const url = 'https://reqres.in/api/users';
+//   return new Promise((resolve, reject) => {
+//     const request = https.get(url, response => {
+//       let rawData = '';
+//       response.on('data', data => {
+//         rawData += data;
+//       });
+//       response.on('end', () => {
+//         try {
+//           resolve(JSON.parse(rawData));
+//         } catch (err) {
+//           reject(new Error(err));
+//         }
+//       });
+//     });
+//     request.on('error', (err: string | undefined) => {
+//       reject(new Error(err));
+//     });
+//   });
+// }
+function usersList(event) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            let url = 'https://reqres.in/api/users';
+            let res = yield axios_1.default.get(url);
+            let jsonData = res.data['data'];
+            //   let name=jsonData['first_name']
+            // let email=jsonData['email']
+            // let avatar=jsonData.avatar
+            // console.log(name);
+            //   console.log(email);
+            //   console.log(avatar);
+            // console.log(jsonData)
+            // var name=jsonData.map((obj: { first_name: any; })=>obj.first_name)
+            // var email=jsonData.map((obj: { email: any; })=>obj.email)
+            // var avatar=jsonData.map((obj: { avatar: any; })=>obj.avatar)
+            var name2 = jsonData.map(({ first_name, email, avatar }) => ({ 'first_name': first_name, 'email': email, 'avatar': avatar }));
+            // console.log(name2)
+            // console.log(test)
+            // console.log(test)
+            // console.log(test)
+            // console.log(data);
+            // console.log(data['data']);
+            // console.log(email);
+            // console.log(avatar);
+            // console.log(avatar);
+            return {
+                statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Credentials": true,
+                },
+                msg: "Fetched data successfully..",
+                error: "",
+                data: JSON.stringify(name2
+                // first_name: name,
+                // email:email,
+                // avatar: avatar
+                ),
+            };
+        }
+        catch (e) {
+            console.log("Exception due to :", e);
+            const response = {
+                statusCode: 500,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Credentials": true,
+                },
+                body: JSON.stringify({ msg: "Internal Server Error", data: e.message }),
+            };
+            return response;
+        }
+    });
+}
+exports.usersList = usersList;
 //# sourceMappingURL=handler.js.map
--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,11 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import DBManager from "./db_manager/db_manager";
+import { default as axios } from 'axios';
 import User from "./model/app";
+// import https = require('https');
+// import axios = require("axios");
+import { resolve } from "path";
+import { rejects } from "assert";
 // import { APIGatewayProxyEvent } from '/Users/bindureddykr/Desktop/Aws_Serverless_Demo_Code/nodejs/node_modules/@types/aws-lambda/index';
 export async function getUsers(event: APIGatewayProxyEvent) {
   let dbManager = new DBManager();
@@ -261,4 +266,183 @@ export async function getUserViaId(event: APIGatewayProxyEvent) {
     };
     return response;
   }
+}
+
+// export async function restApi(event:APIGatewayProxyEvent){
+//   try{
+//     // let db= new DBManager();
+//     // db.createDBConnection([User])
+      
+//     let url ='https://jsonplaceholder.typicode.com/todos'
+//     let data= new Promise((resolve, reject)=>{
+//       let request=https.get(url,response=>{
+//         let rawData='';
+//         response.on('data',reqData=>{
+//           rawData=rawData+reqData
+//         });
+//         response.on('end',()=>{
+//           try{
+//             resolve(JSON.stringify(rawData))
+//         }
+//         catch(e){
+//        reject(new Error(e))
+//         }
+//         });
+//       })
+//       request.on('Error',(err:string|undefined)=>{
+//         reject(new Error(err))
+//       });
+//     })
+
+//     return {
+//       statusCode: 200,
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Credentials": true,
+//       },
+//       body: JSON.stringify({
+//         msg: "Fetched data successfully..",
+//         data: data,
+//       }),
+//     };
+
+//   }
+//   catch(e){
+//     console.log("Exception due to :",e)
+//     const response = {
+//       statusCode: 500,
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Credentials": true,
+//       },
+//       body: JSON.stringify({ msg: "Internal Server Error", data: e.message }),
+//     };
+//     return response;
+//   }
+ 
+// }
+
+export async function currentTime(event:any){
+  let response={
+    statusCode:200,
+    hearder:{
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body:JSON.stringify({
+      message: `Hello, the current time is ${new Date().toTimeString()}.`,
+
+    })
+  }
+  return response
+}
+
+// export async function httpApi(event:any)  {
+//   try {
+//     const result = await getRequest();
+//     console.log('result is: ', result);
+//     return {
+//       statusCode: 200,
+//       headers: {
+//         'Content-Type': 'application/json',
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Credentials": true,
+//     },
+//       body: JSON.stringify(result),
+//     };
+//   } catch (error) {
+//     console.log('Error is: ', error);
+//     return {
+//       statusCode: 400,
+//       body: error.message,
+//     };
+//   }
+// };
+
+// function getRequest() {
+
+//   const url = 'https://reqres.in/api/users';
+
+//   return new Promise((resolve, reject) => {
+//     const request = https.get(url, response => {
+//       let rawData = '';
+//       response.on('data', data => {
+//         rawData += data;
+//       });
+
+//       response.on('end', () => {
+//         try {
+//           resolve(JSON.parse(rawData));
+//         } catch (err) {
+//           reject(new Error(err));
+//         }
+//       });
+//     });
+
+//     request.on('error', (err: string | undefined) => {
+//       reject(new Error(err));
+//     });
+//   });
+// }
+
+export async function usersList(event:APIGatewayProxyEvent){
+
+try{
+  let url ='https://reqres.in/api/users';
+  let res = await axios.get(url);
+  let jsonData = res.data['data'];
+  
+//   let name=jsonData['first_name']
+// let email=jsonData['email']
+// let avatar=jsonData.avatar
+// console.log(name);
+//   console.log(email);
+//   console.log(avatar);
+  // console.log(jsonData)
+  // var name=jsonData.map((obj: { first_name: any; })=>obj.first_name)
+  // var email=jsonData.map((obj: { email: any; })=>obj.email)
+  // var avatar=jsonData.map((obj: { avatar: any; })=>obj.avatar)
+
+  var name2=jsonData.map( ({first_name, email,avatar}) => ({'first_name':first_name,'email':email,'avatar':avatar}) )
+  // console.log(name2)
+  // console.log(test)
+  // console.log(test)
+  // console.log(test)
+  // console.log(data);
+  // console.log(data['data']);
+  // console.log(email);
+  // console.log(avatar);
+  // console.log(avatar);
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    msg: "Fetched data successfully..",
+    error:"",
+    data: JSON.stringify(
+        name2
+        // first_name: name,
+			  // email:email,
+			  // avatar: avatar
+
+    ),
+  };
+}
+  
+catch(e){
+  console.log("Exception due to :",e)
+  const response = {
+    statusCode: 500,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify({ msg: "Internal Server Error", data: e.message }),
+  };
+  return response;
+}
+
 }
