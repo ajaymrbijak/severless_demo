a5fd6672fdb91726cd1bcbbbeeef7a0f1eb67ea1
ajaymrbijak <ajay.madireddy@bijak.in>
2022-10-02 17:27:03 +0530

    New Api

--- src/handler.ts
+++ src/handler.ts
@@ -268,122 +268,6 @@ export async function getUserViaId(event: APIGatewayProxyEvent) {
   }
 }
 
-// export async function restApi(event:APIGatewayProxyEvent){
-//   try{
-//     // let db= new DBManager();
-//     // db.createDBConnection([User])
-      
-//     let url ='https://jsonplaceholder.typicode.com/todos'
-//     let data= new Promise((resolve, reject)=>{
-//       let request=https.get(url,response=>{
-//         let rawData='';
-//         response.on('data',reqData=>{
-//           rawData=rawData+reqData
-//         });
-//         response.on('end',()=>{
-//           try{
-//             resolve(JSON.stringify(rawData))
-//         }
-//         catch(e){
-//        reject(new Error(e))
-//         }
-//         });
-//       })
-//       request.on('Error',(err:string|undefined)=>{
-//         reject(new Error(err))
-//       });
-//     })
-
-//     return {
-//       statusCode: 200,
-//       headers: {
-//         "Access-Control-Allow-Origin": "*",
-//         "Access-Control-Allow-Credentials": true,
-//       },
-//       body: JSON.stringify({
-//         msg: "Fetched data successfully..",
-//         data: data,
-//       }),
-//     };
-
-//   }
-//   catch(e){
-//     console.log("Exception due to :",e)
-//     const response = {
-//       statusCode: 500,
-//       headers: {
-//         "Access-Control-Allow-Origin": "*",
-//         "Access-Control-Allow-Credentials": true,
-//       },
-//       body: JSON.stringify({ msg: "Internal Server Error", data: e.message }),
-//     };
-//     return response;
-//   }
- 
-// }
-
-export async function currentTime(event:any){
-  let response={
-    statusCode:200,
-    hearder:{
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
-    },
-    body:JSON.stringify({
-      message: `Hello, the current time is ${new Date().toTimeString()}.`,
-
-    })
-  }
-  return response
-}
-
-// export async function httpApi(event:any)  {
-//   try {
-//     const result = await getRequest();
-//     console.log('result is: ', result);
-//     return {
-//       statusCode: 200,
-//       headers: {
-//         'Content-Type': 'application/json',
-//         "Access-Control-Allow-Origin": "*",
-//         "Access-Control-Allow-Credentials": true,
-//     },
-//       body: JSON.stringify(result),
-//     };
-//   } catch (error) {
-//     console.log('Error is: ', error);
-//     return {
-//       statusCode: 400,
-//       body: error.message,
-//     };
-//   }
-// };
-
-// function getRequest() {
-
-//   const url = 'https://reqres.in/api/users';
-
-//   return new Promise((resolve, reject) => {
-//     const request = https.get(url, response => {
-//       let rawData = '';
-//       response.on('data', data => {
-//         rawData += data;
-//       });
-
-//       response.on('end', () => {
-//         try {
-//           resolve(JSON.parse(rawData));
-//         } catch (err) {
-//           reject(new Error(err));
-//         }
-//       });
-//     });
-
-//     request.on('error', (err: string | undefined) => {
-//       reject(new Error(err));
-//     });
-//   });
-// }
 
 export async function usersList(event:APIGatewayProxyEvent){
 
@@ -391,29 +275,12 @@ try{
   let url ='https://reqres.in/api/users';
   let res = await axios.get(url);
   let jsonData = res.data['data'];
-  
-//   let name=jsonData['first_name']
-// let email=jsonData['email']
-// let avatar=jsonData.avatar
-// console.log(name);
-//   console.log(email);
-//   console.log(avatar);
-  // console.log(jsonData)
-  // var name=jsonData.map((obj: { first_name: any; })=>obj.first_name)
-  // var email=jsonData.map((obj: { email: any; })=>obj.email)
-  // var avatar=jsonData.map((obj: { avatar: any; })=>obj.avatar)
-
-  var name2=jsonData.map( ({first_name, email,avatar}) => ({'first_name':first_name,'email':email,'avatar':avatar}) )
-  // console.log(name2)
-  // console.log(test)
-  // console.log(test)
-  // console.log(test)
-  // console.log(data);
-  // console.log(data['data']);
-  // console.log(email);
-  // console.log(avatar);
-  // console.log(avatar);
-
+  var result=jsonData.map( ({first_name, email,avatar}) => ({first_name,email,avatar}) )
+  // console.log(result)
+  // let url2='https://jsonplaceholder.typicode.com/todos'
+  // let res1 = await axios.get(url2);
+  // let jsonData2 = res1.data.map(({title,completed})=>({title,completed}));
+  // console.log(jsonData2)
   return {
     statusCode: 200,
     headers: {
@@ -422,13 +289,7 @@ try{
     },
     msg: "Fetched data successfully..",
     error:"",
-    data: JSON.stringify(
-        name2
-        // first_name: name,
-			  // email:email,
-			  // avatar: avatar
-
-    ),
+    data: JSON.stringify(result),
   };
 }
   
